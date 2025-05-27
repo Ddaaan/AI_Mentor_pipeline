0f6994508be455912fe0849261ea02833ec3a07d
Ddaaan <de2572@naver.com>
2025-05-27 15:43:16 +0900

    Ai_Mentor_pipeline 설명 주석 추가

--- Ai_Mentor_pipeline.py
+++ Ai_Mentor_pipeline.py
@@ -16,6 +16,7 @@ class Pipeline:
         self.valves = self.Valves()
         pass
 
+    # 파이프 라인 이름을 설정, 위에서 정의한 Valves 객체 초기화
     async def on_startup(self):
         print(f"on_startup:{__name__}")
 
@@ -27,12 +28,14 @@ class Pipeline:
         self,
         user_message: str, # 사용자 입력 메시지
         model_id: str,
-        messages: List[dict], # 전체 대화 히스토리리
+        messages: List[dict], # 전체 대화 히스토리
         body: dict
     ) -> Union[str, Generator, Iterator]:
 
+        # ----------------- Debug line ----------------------
         print(f"[PIPE] user_message: {user_message}")
         print(f"[PIPE] messages: {messages}")
+        # ---------------------------------------------------
 
         # FastAPI에 보낼 입력 추출 (사용자 메시지 추출)
         last_user_msg = ""
@@ -40,7 +43,7 @@ class Pipeline:
             if m.get("role") == "user":
                 last_user_msg = m.get("content", "")
 
-        print(f"[PIPE] 마지막 유저 메시지: {last_user_msg}")
+        print(f"[PIPE] 마지막 유저 메시지: {last_user_msg}") # Debug line
 
         try:
             r = requests.post(
@@ -50,8 +53,8 @@ class Pipeline:
             )
             r.raise_for_status()
 
-            result = r.json()
-            msg = result.get("message", {})
+            result = r.json() # json 형태의 응답을 가져옴
+            msg = result.get("message", {}) # 그중 message 키를 가져옴
 
             # 요약 텍스트 만들기
             final_message = msg.get("final_message")
@@ -60,9 +63,10 @@ class Pipeline:
             # 문자열로 변환
             summary_text = ""
 
-            if isinstance(final_message, dict):
+            # isinstance : 특정 값이 특정 클래스나 자료형의 인스턴스인지 확인 (isinstance (객체, 클래스) -> T/F 반환함)
+            if isinstance(final_message, dict): # final_message가 dict 자료형이면
                 summary_text += "최종 추천 결과:\n"
-                summary_text += json.dumps(final_message, ensure_ascii=False) + "\n\n"
+                summary_text += json.dumps(final_message, ensure_ascii=False) + "\n\n" # ensure_ascii : 한글 깨짐 방지
 
             if isinstance(steps, list):
                 for step in steps:
@@ -71,7 +75,7 @@ class Pipeline:
                     summary_text += f"응답: {json.dumps(step['tool_response'], ensure_ascii=False)}\n" # 응답값 출력 dict 형태인데 dumps를 활용해 json 문자열 형태로 변환해서 출력력 (dict : 딕셔너리 자료형 - key-value쌍의 집합)
                     summary_text += f"사유: {step['reason']}\n\n" # LLM이 이 도구를 사용한 이유
 
-            return summary_text.strip()
+            return summary_text.strip() # 전체 요약된 문자열 반환 (앞뒤 공백을 제거하고고)
 
         except Exception as e:
             return f"FastAPI 호출 오류: {str(e)}"
